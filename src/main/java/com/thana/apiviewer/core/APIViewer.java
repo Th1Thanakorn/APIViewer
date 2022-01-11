9f91408cdd6ea189a159bbc70c797154a8d78237
IHCXNT4_43UI\uSEr <thanakorn14064@gmail.com>
2022-01-11 09:11:53 +0700

    First Commit

---
+++ src/main/java/com/thana/apiviewer/core/APIViewer.java
@@ -0,0 +1,34 @@
+package com.thana.apiviewer.core;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(APIViewer.MOD_ID)
+public class APIViewer {
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "apiviewer";
+
+    public APIViewer() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) {
+    }
+}
